8d57ebf089d04f609f9955920a3177d577949866
Oleg Dosov <user@24-003222.iteco-inno.ru>
2024-01-21 14:13:07 +0300

    first smart contract

---
+++ smart-contract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract Inbox {
+    string public message;
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
