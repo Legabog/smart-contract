25b73603eef7c63678f23d8faadbcde5d20536cf
Oleg Dosov <user@24-003222.iteco-inno.ru>
2024-01-22 14:08:45 +0300

    chng

--- contracts/inbox.sol
+++ contracts/inbox.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.22;
 contract Inbox {
     string public message;
 
-    function setMessage(string memory newMessage) public {
+    function setMessage(string memory newMessage) public view {
         message = newMessage;
     }
 }
